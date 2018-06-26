610d8a43e845779aa460d6098e42b778e0509a47
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2018-06-26 23:40:30 +0900

    最初のコミット

---
+++ CreateControls/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CreateControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Height = Screen.PrimaryScreen.Bounds.Height - 100;
+            Width = Screen.PrimaryScreen.Bounds.Width - 50;
+            StartPosition = FormStartPosition.CenterScreen;
+            int count = 1;
+            var tip = new ToolTip(components);
+            try
+            {
+                const int hh = 20;
+                int xMax = Width / hh - 3;
+                int yMax = Height / hh - 4;
+                for (var y = 1; y <= yMax; y += 1)
+                    for (var x = 1; x <= xMax; x += 1)
+                    {
+                        var ctl = new Button();
+                        ctl.Size = new Size(hh, hh);
+                        ctl.Location = new Point(x * hh, y * hh);
+                        ctl.Click += btn_click;
+                        tip.SetToolTip(ctl, count.ToString());
+                        Controls.Add(ctl);
+                        count += 1;
+                    }
+            }
+            catch (Win32Exception) { }
+        }
+        private void btn_click(object sender, System.EventArgs e)
+        {
+            var ctl = (Button)sender;
+            ctl.BackColor = Color.Black;
+        }
+    }
+}
---
+++ WPFCreateControls/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPFCreateControls
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Height = SystemParameters.PrimaryScreenHeight - 100;
+            Width = SystemParameters.PrimaryScreenWidth - 50;
+            Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+            Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
+            int count = 1;
+            const double hh = 20;
+            double xMax = Width / hh - 3;
+            double yMax = Height / hh - 4;
+            for (var y = 1; y <= yMax; y += 1)
+                for (var x = 1; x <= xMax; x += 1)
+                {
+                    var ctl = new Button();
+                    ctl.Width = hh;
+                    ctl.Height = hh;
+                    ctl.SetValue(Canvas.LeftProperty, x * hh);
+                    ctl.SetValue(Canvas.TopProperty, y * hh);
+                    ctl.Click += btn_click;
+                    var tip = new ToolTip();
+                    tip.Content = count.ToString();
+                    ctl.SetValue(Canvas.ToolTipProperty, tip);
+                    canv.Children.Add(ctl);
+                    count += 1;
+                }
+        }
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            var ctl = (Button)sender;
+            ctl.Background = new SolidColorBrush(Colors.Black);
+        }
+    }
+}
